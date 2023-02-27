b0c93ba3769109f8bbe0349912e8d2be50b7691f
Zach Peter <peterz@bc.edu>
2023-02-27 16:01:19 -0500

    finished 5.2

---
+++ ToDoList/DetailView.swift
@@ -0,0 +1,41 @@
+//
+//  DetailView.swift
+//  ToDoList
+//
+//  Created by Zach Peter on 2/27/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    @Environment(\.dismiss) private var dismiss
+    
+    var passedValue: String
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(.orange)
+            Text("You are a swifty legend!\nAnd you passed over the value \(passedValue)")
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
+            
+            Spacer()
+            
+            Button("Get Back") {
+                dismiss()
+            }
+            .buttonStyle(.borderedProminent)
+        }
+        .padding()
+        
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(passedValue: "Item 1")
+    }
+}
